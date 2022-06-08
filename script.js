4b3c16a7dce5eedb262cfab3156938be5504bbf3
Rafael Aguiar <terule@gmail.com>
2022-06-08 15:42:46 -0300

    Feat: estilos CSS e requisito 4 que estava com problema

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-function login() {
+function login(event) {
+  event.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   const validLogin = email === 'tryber@teste.com' && password === '123456';
   if (validLogin) {
-    alert('Olá,Tryber!');
+    alert('Olá, Tryber!');
   } else {
-    alert('Email ou senha inválidos');
+    alert('Email ou senha inválidos.');
+  }
+}
+
+function checkAgreement() {
+  const submitForm = document.getElementById('submit-btn');
+  const agreement = document.getElementById('agreement');
+  if (agreement.checked) {
+    submitForm.disabled = false;
+  } else {
+    submitForm.disabled = true;
   }
 }
 
 const submit = document.getElementById('submit');
 submit.addEventListener('click', login);
+
+const agreement = document.getElementById('agreement');
+agreement.addEventListener('change', checkAgreement);
+
+const submitForm = document.getElementById('submit-btn');
+submitForm.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+const textArea = document.getElementById('textarea');
+textArea.addEventListener('keypress', () => {
+  const text = document.getElementById('textarea');
+  const counterText = document.getElementById('counter');
+  counterText.innerText = text.maxLength - text.value.length;
+});
